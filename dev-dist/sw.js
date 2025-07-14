d041e82b1a09472faea43108db76828b95cb861e
GymBroSystem2025 <kauancerniauskasaraujo@hotmail.com>
2025-07-13 23:27:35 -0300

    logo att

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o5h8vjn4qkg"
+    "revision": "0.3ikm235r8k8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
